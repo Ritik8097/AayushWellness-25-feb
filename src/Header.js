e94d6df771bf96ddddeac29e30dc5c65408cb79c
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-02-26 13:35:19 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -159,8 +159,8 @@ export default function Header() {
 
               <Link to="/career" className=" hover:text-primary/80">Careers</Link>
               <Link to="/support" className=" hover:text-primary/80">Support</Link>
-              <Link to="/health" className="block  px-4  hover:text-primary/80"> Health</Link>
-              <Link  to="/growth-accelerator" className="block  px-4  hover:text-primary/80">Accelerator</Link>
+              <Link to="/health" className="block  hover:text-primary/80"> Health</Link>
+              <Link  to="/growth-accelerator" className="block  hover:text-primary/80">Accelerator</Link>
               
             
 
