de56f1d45f507cc04e4a04ebfa6e3c7b8ae2cde4
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-03-05 15:37:39 +0530

    Update Asection1.js

--- src/Asection1.js
+++ src/Asection1.js
@@ -9,7 +9,7 @@ const Asection1 = () => {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1.8, ease: "easeInOut" }}
       viewport={{ once: true }}
-      className="flex flex-col items-center justify-center px-4 pt-40 text-center max-w-[1200px] mx-auto"
+      className="flex flex-col items-center justify-center px-4 pt-40 text-center max-w-auto mx-auto"
     >
       {/* Title: "Our Story" */}
       <h1 
@@ -22,19 +22,21 @@ const Asection1 = () => {
       {/* Story Content */}
       <div className="text-[28px] md:text-[20px] leading-[155%] text-[#3a4458] max-w-[800px]">
         <p className="font-[28px]">
-          <span className="font-bold text-black">Bringing the Future of Wellness to You</span>
+          <span className=" text-[30px]  text-black">Bringing the Future of Wellness to You</span>
           <br />
-          At <strong className="font-semibold text-black">Aayush Wellness,</strong> we believe that <strong className="font-bold text-black">health is not just about curing
+          <span className="font-semibold pt-5"> 
+          At Aayush Wellness, we believe that health is not just about curing
           illness—it’s about preventing it, enhancing it, and making wellness a
-          way of life.</strong>
+          way of life.
+          </span>
         </p>
         <br />
-        <p>
-          With a deep-rooted commitment to <strong className="font-semibold text-black">science-backed innovation and natural
-          health solutions</strong>, we are redefining the <strong className="font-bold text-black">wellness
-          experience</strong>—integrating <strong className="font-semibold text-black">nutraceuticals, personalized healthcare,
-          preventive wellness</strong>, and digital health solutions to create a seamless
-          journey toward <strong className="font-semibold text-black">better living.</strong>
+        <p className="font-semibold">
+          With a deep-rooted commitment to science-backed innovation and natural
+          health solutions, we are redefining the wellness
+          experience—integrating nutraceuticals, personalized healthcare,
+          preventive wellness, and digital health solutions to create a seamless
+          journey toward better living.
         </p>
       </div>
     </motion.div>
