9b4af3cd86b7d26546ec157326bc43e5e8297e89
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-03-08 17:29:41 +0530

    Update Slider.js

--- src/Slider.js
+++ src/Slider.js
@@ -739,7 +739,7 @@ export default function AnimatedSlider() {
         <div
   className="absolute md:bottom-[370px] bottom-[440px] left-4  md:left-4 text-left text-[#ffebc4] font-sm z-20"
   style={{
-    fontSize: window.innerWidth < 768 ? "16px" : "16px", left: "0.5rem"  // 20px for small screens, 24px for larger screens
+    fontSize: window.innerWidth < 768 ? "18px" : "16px", left: "0.5rem"  // 20px for small screens, 24px for larger screens
   }}
 >
   Services/Products
@@ -777,13 +777,13 @@ export default function AnimatedSlider() {
         <div
   className="absolute bottom-[130px] left-4 md:left-4 text-left text-[#ffebc4] leading-tight font-sm z-20 md:hidden"
   style={{
-    fontSize: window.innerWidth < 768 ? "16px" : "16px", left: "0.5rem" // 10px for mobile, 20px for desktop
+    fontSize: window.innerWidth < 768 ? "18px" : "16px", left: "0.5rem" // 10px for mobile, 20px for desktop
   }}
 >
   Introduction
 </div>
 
-        <div className="absolute bottom-[20px] left-4 right-4 text-center text-[#ffebc4]  leading-tight font-meduim z-20 md:hidden" style={{
+        <div className="absolute bottom-[10px] left-4 right-4 text-center text-[#ffebc4]  leading-tight font-bold z-20 md:hidden" style={{
     fontSize: window.innerWidth < 768 ? "14px" : "20px",   // 20px for small screens, 24px for larger screens
   }}>
           We prioritise prevention over treatment, offering healthcare, science-backed products, and holistic wellness empowering individuals towards lifelong well-being.
