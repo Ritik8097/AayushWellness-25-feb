e4bcc881cfa0e18e73e07459d46a8ca75ccb431e
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-03-12 18:36:31 +0530

    Update NewFooter.js

--- src/NewFooter.js
+++ src/NewFooter.js
@@ -36,7 +36,7 @@ const NewFooter = () => {
   }
 
   return (
-    <footer className="relative w-full md:h-[80vh] overflow-hidden text-white font-sans">
+    <footer className="relative w-full md:h-[80vh] overflow-hidden text-white font-sans blocks">
       <video className="absolute top-0 left-0 w-full h-full object-cover z-0" autoPlay loop muted>
         <source src={videoSrc} type="video/mp4" />
         Your browser does not support the video tag.
