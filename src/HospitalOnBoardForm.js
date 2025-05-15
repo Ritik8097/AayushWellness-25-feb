2f8878aaff3d71e407c6f5b9e240b75435a657ad
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-05-15 18:39:47 +0530

    Update HospitalOnBoardForm.js

--- src/HospitalOnBoardForm.js
+++ src/HospitalOnBoardForm.js
@@ -1,4 +1,4 @@
-"use client"
+
 
 import { useState } from "react"
 import { useLocation } from "react-router-dom"
@@ -488,7 +488,7 @@ const HospitalOnBoardForm = () => {
                     animation: "fadeIn 0.5s ease forwards 0.3s",
                   }}
                 >
-                  <h1 className="font-bold text-[#233f8f] text-3xl sm:text-4xl mb-2">
+                  <h1 className="font-bold !text-[#233f8f] text-3xl sm:text-4xl mb-2">
                     Healthcare Hospital Registration
                   </h1>
                   <p className="text-gray-600 mb-1 max-w-2xl mx-auto">
