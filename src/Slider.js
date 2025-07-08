c3159e1ba7f6cf7fbbd7e9631f5726e66b7a9a6c
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-07-08 18:11:25 +0530

    Update Slider.js

--- src/Slider.js
+++ src/Slider.js
@@ -422,6 +422,15 @@ export default function AnimatedSlider() {
                         Partner with us for sustainable growth
                       </span>
                     </Link>
+                        <Link
+                      to="#"
+                      className="grid grid-cols-1 text-left py-2 text-black rounded-md !text-[#004037] hover:bg-[#004037] transition w-full hover:!text-white"
+                    >
+                      <span className="block font-bold w-full px-4 text-inherit">press Releases</span>
+                      <span className="block text-sm px-4 text-gray-900 text-inherit">
+                         Stay updated with our latest announcements and milestones
+                      </span>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -749,6 +758,14 @@ export default function AnimatedSlider() {
                     >
                       Investors
                       <p className="text-sm text-gray-600">Partner with us for sustainable growth</p>
+                    </Link>
+                         <Link
+                      to="#"
+                      className="block px-4 py-3 text-[#004037] font-bold "
+                      onClick={() => setIsCorporateDropdownOpen(false)}
+                    >
+                      Press Releases
+                      <p className="text-sm text-gray-600">Stay updated with our latest announcements and milestones</p>
                     </Link>
                   </div>
                 </div>
