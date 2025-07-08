884bb7adfbfd67e5cb348a7606913d19619c811d
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-07-08 18:20:19 +0530

    Update MissionHeader.js

--- src/MissionHeader.js
+++ src/MissionHeader.js
@@ -557,6 +557,17 @@ export default function AnimatedSlider() {
                         Partner with us for sustainable growth
                       </span>
                     </Link>
+                         <Link
+                      to="#s"
+                      className="grid grid-cols-1 text-left py-2 text-black rounded-md !text-[#004037] hover:bg-[#004037] transition w-full hover:!text-white"
+                    >
+                      <span className="block font-bold w-full px-4 text-inherit">
+                        Press Releases
+                      </span>
+                      <span className="block text-sm px-4 text-gray-900 text-inherit">
+                         Stay updated with our latest announcements and milestones
+                      </span>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -1043,6 +1054,14 @@ export default function AnimatedSlider() {
         >
           Investors
           <p className="text-sm text-gray-600">Partner with us for sustainable growth</p>
+        </Link>
+            <Link
+          to="#"
+          className="block px-4 py-3 text-[#004037] font-bold "
+          onClick={() => setIsCorporateDropdownOpen(false)}
+        >
+          Press Releases
+          <p className="text-sm text-gray-600"> Stay updated with our latest announcements and milestones</p>
         </Link>
       </div>
     </div>
