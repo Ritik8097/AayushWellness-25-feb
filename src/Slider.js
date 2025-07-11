62906899278cc0a8d3f99179f5d267496179183f
Ritik8097 <98981330+Ritik8097@users.noreply.github.com>
2025-07-11 11:27:07 +0530

    Update Slider.js

--- src/Slider.js
+++ src/Slider.js
@@ -423,7 +423,7 @@ export default function AnimatedSlider() {
                       </span>
                     </Link>
                         <Link
-                      to="#"
+                      to="/press-releases"
                       className="grid grid-cols-1 text-left py-2 text-black rounded-md !text-[#004037] hover:bg-[#004037] transition w-full hover:!text-white"
                     >
                       <span className="block font-bold w-full px-4 text-inherit">Press Releases</span>
@@ -760,7 +760,7 @@ export default function AnimatedSlider() {
                       <p className="text-sm text-gray-600">Partner with us for sustainable growth</p>
                     </Link>
                          <Link
-                      to="#"
+                      to="/press-releases"
                       className="block px-4 py-3 text-[#004037] font-bold "
                       onClick={() => setIsCorporateDropdownOpen(false)}
                     >
